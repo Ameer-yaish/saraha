ceb2ee56cbbe3265e2c138730781b57fa80871c1
ameer yaesh <ameer1momo1@gmail.com>
2022-12-10 11:17:35 +0200

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose=require("mongoose")
 const app = express()
 const port = 3000
-mongoose.connect("mongodb://127.0.0.1:27017/Honest")
+mongoose.connect("mongodb+srv://ameer:ameer123@cluster0.4urwsng.mongodb.net/Honest")
 app.use(express.json())
 app.use("/user",require("./api/user.api"))
 
