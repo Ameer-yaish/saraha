0ab532b3597c964477d2bb760368735c1b0a8240
ameer yaesh <ameer1momo1@gmail.com>
2022-12-10 11:23:18 +0200

    home message

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const port = 3000
 mongoose.connect("mongodb+srv://ameer:ameer123@cluster0.4urwsng.mongodb.net/Honest")
 app.use(express.json())
 app.use("/user",require("./api/user.api"))
+app.get('/',(req,res,next)=>{
+res.json("welcome")
 
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
